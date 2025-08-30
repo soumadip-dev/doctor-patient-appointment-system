bf25619ab7545f8b8c14565038ef0da302a68474
soumadip-dev <soumadipmajila@gmail.com>
2025-08-30 22:19:14 +0530

    more chnage

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const ENV = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
-  BASE_URL: process.env.BASE_URL,
+  BASE_URL: 'https://doctor-patient-appointment-system-rucz.onrender.com/',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
   NODE_ENV: process.env.NODE_ENV,
